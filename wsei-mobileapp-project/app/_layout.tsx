7496b6167bfe64c9851707d1b4d999be9d52123c
RodionSavenko <savenko662@gmail.com>
2024-04-29 00:48:27 +0200

    Early implementation

---
+++ wsei-mobileapp-project/app/_layout.tsx
@@ -0,0 +1,54 @@
+import { Slot } from "expo-router";
+import React from "react";
+import { View, Text, StyleSheet, Platform, StatusBar } from "react-native";
+
+const Layout = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.appBar}>
+        <Text style={styles.headerText}>React zaliczeniowy</Text>
+        <View style={styles.linkContainer}>
+          <Text style={styles.link}>Posts</Text>
+          <Text style={styles.link}>Users</Text>
+        </View>
+      </View>
+      <View style={styles.contentContainer}>
+        <Slot />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  appBar: {
+    backgroundColor: "lightblue",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  linkContainer: {
+    flexDirection: "row",
+  },
+  link: {
+    marginLeft: 20,
+    fontSize: 16,
+    color: "blue",
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default Layout;
---
+++ wsei-mobileapp-project/app/lol/index.tsx
@@ -0,0 +1,25 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Page() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!!!</Text>
+      <StatusBar style="auto" />
+      <Link href="/">About</Link>
+      <Link href="/Lol/">About</Link>
+      {/* ...other links */}
+      {/* <Link href="/src/">View user</Link> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
